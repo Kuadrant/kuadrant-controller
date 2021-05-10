9e2c3f4e6e72e0fdc1ffdc7bbc18318683ff0b16
Joaquim Moreno <jmprusi@keepalive.io>
2021-05-10 11:29:47 +0200

    Added APIProduct controller

--- controllers/networking/suite_test.go
+++ controllers/networking/suite_test.go
@@ -62,6 +62,9 @@ var _ = BeforeSuite(func() {
 	err = networkingv1beta1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
+	err = networkingv1beta1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
 	// +kubebuilder:scaffold:scheme
 
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
